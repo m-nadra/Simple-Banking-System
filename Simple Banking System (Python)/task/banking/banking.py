23f883b7d4b90e0764f77120869dabab6a37eeab
m-nadra <m.nadra@proton.me>
2023-10-27 19:13:56 +0200

    add pin generator, make dictionary save users data

--- Simple Banking System (Python)/task/banking/banking.py
+++ Simple Banking System (Python)/task/banking/banking.py
@@ -6,11 +6,19 @@ def card_number_generator():
     client_id = random.randint(100000000, 999999999)
     card_number = "400000" + str(client_id) + "2"
     print(card_number)
+    return card_number
 
 
 def pin_generator():
-    print(random.randint(1000, 9999))
+    card_pin_number = []
+    for i in range(4):
+        card_pin_number.append(random.randint(0, 9))
+    card_pin_number = ''.join(map(str, card_pin_number))
+    print(card_pin_number)
+    return card_pin_number
 
+
+bank_accounts = {}
 
 while True:
     print("\n1. Create an account")
@@ -22,10 +30,10 @@ while True:
         case 1:
             print("Your card has been created")
             print("Your card number:")
-            card_number_generator()
+            client_card_number = card_number_generator()
             print("Your card PIN:")
-            pin_generator()
-
+            client_card_pin_number = pin_generator()
+            bank_accounts[client_card_number] = client_card_pin_number
         case 2:
             pass
         case 0:
