5ecbd4020dde9175360b032e954f37595785136c
m-nadra <m.nadra@proton.me>
2023-10-27 19:32:27 +0200

    add login to account option

--- Simple Banking System (Python)/task/banking/banking.py
+++ Simple Banking System (Python)/task/banking/banking.py
@@ -1,5 +1,6 @@
 # Write your code here
 import random
+import sys
 
 
 def card_number_generator():
@@ -35,10 +36,32 @@ while True:
             client_card_pin_number = pin_generator()
             bank_accounts[client_card_number] = client_card_pin_number
         case 2:
-            pass
+            print("Enter your card number:")
+            login_card_number = input(">")
+            print("Enter your PIN:")
+            login_card_pin_number = input(">")
+            if login_card_number in bank_accounts and bank_accounts[login_card_number] == login_card_pin_number:
+                print("You have successfully logged in!")
+                while True:
+                    print("\n1. Balance")
+                    print("2. Log out")
+                    print("0. Exit")
+                    logged_user_input = int(input(">"))
+                    match logged_user_input:
+                        case 1:
+                            print("Balance: 0")
+                        case 2:
+                            print("You have successfully logged out!")
+                            break
+                        case 0:
+                            print("Bye!")
+                            sys.exit()
+                        case _:
+                            print("Invalid input! Try again.")
+            else:
+                print("Wrong card number or PIN!")
         case 0:
             print("Bye!")
             break
         case _:
             print("Invalid input! Try again.")
-            continue
